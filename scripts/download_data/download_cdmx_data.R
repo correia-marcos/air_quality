441c790484eb0ec0d25dc061e454b7ad3ea9b63a
correia-marcos <marcospaulorcorreia@gmail.com>
2025-09-19 15:43:22 -0300

    Updates in CDMX downloading to download ground stations data from states near metro area

--- scripts/download_data/download_cdmx_data.R
+++ scripts/download_data/download_cdmx_data.R
@@ -45,34 +45,50 @@ metro_area <- cdmx_download_metro_area(
   quiet             = FALSE
 )
 
-# Apply function to generate and save dataframe with stations and their location
+# Apply function to gen/save table with stations and their location - inside CDMX
 station_in_cdmx <- cdmx_scrape_station_catalog(
   page_url      = cdmx_cfg$url_loc_stations_cdmx,
   out_dir       = here::here(cdmx_cfg$out_dir, "pollution_ground_stations", "Mexico_city"),
   out_name      = "cdmx_station_location",
-  write_parquet = FALSE,   # or FALSE if you don’t want a file
+  write_parquet = FALSE,
   write_csv     = TRUE,
-  write_rds     = FALSE
+  write_rds     = FALSE,
+  verbose       = TRUE
 )
 
-# Apply function to create Selenium server and download the data for Bogota
-cdmx_download_sinaica_data(
-  base_url      = cdmx_cfg$base_url_sinaica,
-  years         = cdmx_cfg$years,
-  container     = TRUE,
-  max_attempts  = 5,
-  timeout_page  = 300,
-  timeout_dl    = 600,
-  subdir        = here::here(cdmx_cfg$dl_dir, "Ground_stations")
+# Apply function to gen/save remain stations location and information - out CDMX
+all_stations <- cdmx_scrape_states_merge(
+  station_in_cdmx = station_in_cdmx,
+  base_url        = cdmx_cfg$url_loc_stations_others,
+  states          = c("Guerrero", "Hidalgo", "México", "Michoacán", "Morelos", "Querétaro",
+                      "Puebla", "Tlaxcala"),
+  out_dir         = here::here(cdmx_cfg$out_dir, "pollution_ground_stations", "Mexico_city"),
+  out_name        = "all_station_location",
+  write_parquet   = FALSE,
+  write_csv       = TRUE,
+  write_rds       = FALSE,
+  verbose         = TRUE
 )
 
-
+# Apply function to create Selenium server and download the data for Mexico stations
+download_logs <- cdmx_download_sinaica_data(
+  base_url                    = cdmx_cfg$base_url_sinaica,
+  years                       = cdmx_cfg$years,
+  container                   = TRUE,
+  timeout_csv                 = 20,
+  settle_before_csv_click_sec = 1,
+  subdir                      = here::here(cdmx_cfg$dl_dir, "Ground_stations")
+)
+# Save the log of downloading for transparency
+write.csv(download_logs, file = path(cdmx_cfg$dl_dir, "stations_log.csv"))
 
 # Apply function to download Census data for the metro area
-census <- bogota_download_census_data(
-  type            = "AMPLIADO",
-  url             = bogota_cfg$base_url_census,
-  download_folder = here::here(bogota_cfg$dl_dir, "census")
+census_log <- cdmx_download_census_data(
+  areas    = c("Ciudad de México", "Hidalgo", "México"),
+  base_url = cdmx_cfg$base_url_census,
+  out_dir  = here::here(cdmx_cfg$dl_dir, "census"),
+  retries  = 5,
+  quiet    = FALSE
 )
 
 # Print a success message for when running inside Docker Container
