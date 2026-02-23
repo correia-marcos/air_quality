d2ca2cbec39051e76e2d57a7bb835e292f0c02db
correia-marcos <marcospaulorcorreia@gmail.com>
2026-02-23 10:25:52 -0300

    Applied new function and changed function in São Paulo downloading script

--- scripts/download_data/download_sao_paulo.R
+++ scripts/download_data/download_sao_paulo.R
@@ -40,19 +40,37 @@ print(sao_paulo_cfg$out_dir)
 # ============================================================================================
 # I: Download data
 # ============================================================================================
-# Apply function to download shapefiles for São Paulo metro area
-sao_paulo_metro <- sao_paulo_download_metro_area(
-  type              = "municipalities",
+# Apply function to download shapefiles for São Paulo metro area - municipalities
+sao_paulo_metro_2010 <- sao_paulo_download_metro_area(
+  level              = "mpio",
   base_url          = sao_paulo_cfg$base_url_shp,
   keep_municipality = sao_paulo_cfg$cities_in_metro,
   download_dir      = here::here(sao_paulo_cfg$dl_dir, "metro_area"),
   out_file          = here::here(sao_paulo_cfg$out_dir, "geospatial_data",
-                                 "sao_paulo", "sao_paulo_metro.gpkg"))
+                                 "sao_paulo", "sao_paulo_metro_2010.gpkg"))
+
+# Apply function to download shapefiles for São Paulo metro area - census tracts
+sao_paulo_metro_2010_census_tracts <- sao_paulo_download_metro_area(
+  level             = "setor_censitario",
+  base_url          = sao_paulo_cfg$base_url_shp,
+  keep_municipality = sao_paulo_cfg$cities_in_metro,
+  download_dir      = here::here(sao_paulo_cfg$dl_dir, "metro_area"),
+  out_file          = here::here(sao_paulo_cfg$out_dir, "geospatial_data",
+                                 "sao_paulo", "sao_paulo_metro_2010_census_tracts.gpkg"))
+
+# Apply function to download shapefiles for São Paulo metro area - weighting areas
+sao_paulo_metro_2010_weighting <- sao_paulo_download_weighting_areas(
+  keep_municipality = sao_paulo_cfg$cities_in_metro,
+  year              = 2010,
+  out_file          = here::here(sao_paulo_cfg$out_dir, "geospatial_data",
+                                 "sao_paulo", "sao_paulo_metro_2010_weighting_areas.gpkg")
+)
+
 
 # Apply function to save a LaTeX table of the states that we must download stations data
 table_states_to_download <- table_state_metro_distances(
   national_states_sf = brazil,
-  metro_area_sf = sao_paulo_metro,
+  metro_area_sf = sao_paulo_metro_2010,
   save_latex_table = TRUE,
   caption = "Administrative states and distance to metropolitan area (in Km)",
   out_file = here::here("results", "tables", "states_to_get_stations", "sao_paulo.tex"),
@@ -67,15 +85,11 @@ write.csv(logs_qualar_stations_hourly_data, row.names = FALSE,
           file = here::here(sao_paulo_cfg$dl_dir, "log_qualar_ground_stations.csv"))
 
 # Apply function to create Selenium server and download the stations metadata
-logs_sinca_stations_metadata    <- santiago_download_station_info(
-  base_url = santiago_cfg$base_url_sinca,
-  subdir   = file.path("santiago", "stations_metadata"))
-
-# Apply function to download Census data for the metro area
-census <- santiago_download_census_data(
-  type            = "geo_location",
-  url             = santiago_cfg$base_url_census,
-  download_folder = here::here(santiago_cfg$dl_dir, "census"))
+logs_qualar_stations_metadata <- sao_paulo_download_metadata(
+  base_url   = sao_paulo_cfg$base_url_qualar,
+  search_url = sao_paulo_cfg$metadata_url_qualar,
+  out_file   = here::here(sao_paulo_cfg$dl_dir,
+                          "stations_metadata", "stations_metadata.csv"))
 
 # Print a success message for when running inside Docker Container
 cat("Script from the IDB projected executed successfully in the Docker container!\n")
