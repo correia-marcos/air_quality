7572193125b462dd2ade78e6bdc785751a572915
correia-marcos <marcospaulorcorreia@gmail.com>
2026-04-12 16:39:35 -0300

    Small change in parameter applied in generate_distances_matrices.R

--- scripts/process_data/generate_distances_matrices.R
+++ scripts/process_data/generate_distances_matrices.R
@@ -34,10 +34,12 @@ gpkg_stations_cdmx            <- here::here(dir_geospatial, "cdmx",
 gpkg_stations_santiago_2024   <- here::here(dir_geospatial, "santiago",
                                             "gran_santiago_stations_buffer_metro_2024.gpkg")
 gpkg_stations_sp_2010         <- here::here(dir_geospatial, "sao_paulo",
-                                            "sao_paulo_stations_buffer_metro_2020.gpkg")
+                                            "sao_paulo_stations_buffer_metro_2010.gpkg")
 
 gpkg_bogota_2018_metro_area   <- here::here(dir_geospatial, "bogota",
                                             "bogota_area_metro_census_tracts_2018.gpkg")
+gpkg_bogota_2018_small        <- here::here(dir_geospatial, "bogota",
+                                            "bogota_area_metro_2018.gpkg")
 gpkg_cdmx_metro_area          <- here::here(dir_geospatial, "cdmx",
                                             "cdmx_area_metro.gpkg")
 gpkg_santiago_2024_metro_area <- here::here(dir_geospatial, "santiago",
@@ -57,6 +59,7 @@ santiago_stations_2024_sf    <- sf::st_read(gpkg_stations_santiago_2024)
 sp_stations_2010_sf          <- sf::st_read(gpkg_stations_sp_2010)
 
 bogota_metro_2018_sf         <- sf::st_read(gpkg_bogota_2018_metro_area)
+bogota_metro_2018_small      <- sf::st_read(gpkg_bogota_2018_small)
 cdmx_metro_sf                <- sf::st_read(gpkg_cdmx_metro_area)
 santiago_metro_2024_sf       <- sf::st_read(gpkg_santiago_2024_metro_area)
 sp_metro_2010_sf             <- sf::st_read(gpkg_sp_2010_metro_area)
@@ -75,7 +78,7 @@ bogota_distances_stations <- compute_distance_matrices(
 bogota_distances_geo_id_stations <- compute_distance_matrices(
   stations_sf    = bogota_stations_2018_sf,
   station_id_col = "station_name",
-  geo_sf         = bogota_metro_2018_sf,
+  geo_sf         = bogota_metro_2018_small,
   geo_id_col     = "GEO_ID",
   out_dir        = outdir_data,
   out_name       = "bogota_2018"
